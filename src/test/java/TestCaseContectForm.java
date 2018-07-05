2fc864bca33670ee6766e20bac740fca43b5b99a
Maksim Khabenko <maksim.k@sbtech.com>
2018-07-05 16:40:13 +0300

    init

--- src/test/java/TestCaseContectForm.java
+++ src/test/java/TestCaseContectForm.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import ru.yandex.qatools.allure.annotations.Features;
+import ru.yandex.qatools.allure.annotations.Issue;
 import ru.yandex.qatools.allure.annotations.Stories;
 import ru.yandex.qatools.allure.annotations.Title;
 
@@ -31,6 +32,7 @@ public class TestCaseContectForm {
     @Title("[Contact form] Checking the \"Name\" field  (Contain Name and surname)")
     @Features("[Contact form]")
     @Stories("Positive")
+    @Issue("Test-1")
     @Test
     public void testCaseP1() throws Exception {
         driver.get("http://firstbridge.io/");
@@ -54,6 +56,7 @@ public class TestCaseContectForm {
     @Title("[Contact form] Sending reqest  with correct data")
     @Features("[Contact form]")
     @Stories("Positive")
+    @Issue("PAAS-2046")
     @Test
     public void testCaseP2() throws Exception {
         driver.get("http://firstbridge.io/");
@@ -97,6 +100,8 @@ public class TestCaseContectForm {
         }
     }
 
+
+    @Issue("PAAS-2046")
     @Title("[Contact form] Checking the \"Select budge\" field 15000-50000 $")
     @Features("[Contact form]")
     @Stories("Positive")
